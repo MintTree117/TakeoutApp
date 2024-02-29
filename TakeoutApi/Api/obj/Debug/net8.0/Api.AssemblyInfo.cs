296b53bab74da112091e91fa25bdd880aa2775af
martin <martygrof3708@gmail.com>
2024-02-29 17:57:51 -0500

    API: Cleaned boilerplate, added configs, setup folder structure.

--- TakeoutApi/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ TakeoutApi/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f2d911449cee5734df5a2d74b12af281c83b4f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
