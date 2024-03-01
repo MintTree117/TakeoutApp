e04c41f32a1ca37e2b6063c7a3ec8f701b8c1781
martin <martygrof3708@gmail.com>
2024-03-01 15:52:42 -0500

    CLIENT: Generated ssl key and configured https.

--- TakeoutApi/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ TakeoutApi/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ccff7fdc67c029aff172819e4ef2efc680392a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50c123b8f48412d099642fab5faa5a58ea237f27")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
