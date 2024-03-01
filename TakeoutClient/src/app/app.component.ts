10db5e3339387da5e2dcc4692ebf7e5e04275d7e
martin <martygrof3708@gmail.com>
2024-03-01 16:30:05 -0500

    Installed boostrap 5 and font awseome. Updated gitignore to accomadate client (I forgot to earlier...)

--- TakeoutClient/src/app/app.component.ts
+++ TakeoutClient/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { setTheme } from 'ngx-bootstrap/utils';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @Component({
   selector: 'app-root',
@@ -10,4 +12,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'TakeoutClient';
+  constructor() {
+    setTheme('bs5');
+  }
 }
